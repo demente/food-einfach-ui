e6eeae6f42acd91cd10a9ee8f62cfe27e2933446
demente <olga.agen@gmail.com>
2018-06-03 12:13:27 +0200

    Add view to list all food

--- src/components/RecipeType.vue
+++ src/components/RecipeType.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="row">
 	<div class="column-sm col-form-label">
-		<TypeTag v-for="type in types" :type="type" />
+		<TypeTag v-for="type in types" :type="type" :key="type.name" />
 	</div>
 </div>
 </template>
--- src/views/FoodListView.vue
+++ src/views/FoodListView.vue
@@ -1,9 +1,42 @@
 <template>
-<div>Here comes list of all food</div>
+<div class="container">
+	<div class="row">
+		<h1>Food</h1>
+	</div>
+	<div class="row">
+		<input class="form-control" placeholder="Filter..."
+			v-model="foodFilter" />
+	</div>
+	<div class="row">&nbsp;</div>
+	<div class="row">
+		<div class="col font-weight-bold ">Name</div>
+		<div class="col font-weight-bold ">Type</div>
+	</div>
+	<FoodRow v-for="food in filteredFoodList" :food="food" :key="food.id" />
+</div>
 </template>
 
 <script>
+import FoodRow from '../components/FoodRow'
+
 export default {
-	 name: 'FoodListView'
+	 name: 'FoodListView',
+	 components: {FoodRow},
+	 data(){
+		 return {
+			 foodList: [{id: 1, name: 'Quinoa', type: 'Grains'},
+				 {id:2, name: 'Milk', type: 'Diary'},
+				 {id:3, name: 'Tomato', type: 'Vegetable'}],
+			foodFilter: ''
+		 }
+	 },
+	 computed: {
+		 filteredFoodList() {
+			 var filterValue = this.foodFilter.toLowerCase()
+			 return this.foodList.filter(function(food) {
+					return food.name.toLowerCase().includes(filterValue) || food.type.toLowerCase().includes(filterValue);
+				});
+		 }
+	 }
 }
 </script>
