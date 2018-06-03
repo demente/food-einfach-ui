760feab2d10cf5216f74ded6e05b4750f137c882
demente <olga.agen@gmail.com>
2018-06-03 12:13:46 +0200

    Add component to show food

---
+++ src/components/FoodRow.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="row">
+<div class="col">{{food.name}}</div>
+<div class="col">{{food.type}}</div>
+</div>
+</template>
+
+<script>
+export default{
+	name: 'FoodRow',
+	props: ['food']
+}
+</script>
