622f374412c6d7c05f7f439223178f1bae25ada9
demente <olga.agen@gmail.com>
2018-06-03 12:20:23 +0200

    Recipe list added

---
+++ src/components/RecipeRow.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="row">
+	<router-link class="nav-item nav-link"
+		:to="{name:'RecipeView', params: {id: recipe.id}}">{{recipe.name}}</router-link>
+</div>
+</template>
+
+<script>
+export default{
+	name: 'RecipeRow',
+	props: ['recipe']
+}
+</script>
--- src/views/RecipeListView.vue
+++ src/views/RecipeListView.vue
@@ -1,15 +1,40 @@
 <template>
-<div>
-	Here comes list of all recipes
-
-	<router-link class="nav-item nav-link"
-		:to="{name:'RecipeView', params: {id: 2}}">Recipe with
-	id 2</router-link>
+<div class="container">
+	<div class="row">
+		<h1>Recipes</h1>
+	</div>
+	<div class="row">
+		<input class="form-control" placeholder="Filter..."
+			v-model="recipeFilter" />
+	</div>
+	<div class="row">&nbsp;</div>
+	<div class="row">
+		<div class="col font-weight-bold ">Name</div>
+	</div>
+	<RecipeRow v-for="recipe in filteredRecipeList" :recipe="recipe"
+		:key="recipe.id" />
 </div>
 </template>
 
 <script>
+import RecipeRow from '../components/RecipeRow'
 export default {
-	 name: 'RecipeListView'
+	 name: 'RecipeListView',
+	 components: {RecipeRow},
+	 data(){
+		 return {
+			 recipeFilter: '',
+			 recipeList: [{id: 1, name: 'Recipe 1'}, {id:2, name: 'Meatballs'}]
+		 }
+	 },
+	 computed: {
+		 filteredRecipeList() {
+			 var filterValue = this.recipeFilter.toLowerCase()
+			 return this.recipeList.filter(function(recipe) {
+					return recipe.name.toLowerCase().includes(filterValue);
+				});
+		 }
+	 }
+
 }
 </script>
