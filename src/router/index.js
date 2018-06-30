f541bcaf81fa2de5322cc0784afb636444de86bf
demente <olga.agen@gmail.com>
2018-06-30 21:54:43 +0200

    Add FoodView route

--- src/router/index.js
+++ src/router/index.js
@@ -4,35 +4,43 @@ import RecipeView from '@/views/RecipeView'
 import RecipeEditableView from '@/views/RecipeEditableView'
 import FoodListView from '@/views/FoodListView'
 import RecipeListView from '@/views/RecipeListView'
+import FoodView from '@/views/FoodView'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-        path: '/recipes/:id',
-        name: 'RecipeView',
-        component: RecipeView,
-        props: true
-      },
-      {
-          path: '/recipes',
-          name: 'RecipeListView',
-          component: RecipeListView,
-          props: true
-       },
-       {
-            path: '/recipes/:id/edit',
-            name: 'RecipeEditableView',
-            component: RecipeEditableView,
-            props: true
-         },
-         {
-              path: '/food',
-              name: 'FoodListView',
-              component: FoodListView,
-              props: true
-            }
+      path: '/recipes/:id',
+      name: 'RecipeView',
+      component: RecipeView,
+      props: true
+    },
+    {
+      path: '/recipes',
+      name: 'RecipeListView',
+      component: RecipeListView,
+      props: true
+    },
+    {
+      path: '/recipes/:id/edit',
+      name: 'RecipeEditableView',
+      component: RecipeEditableView,
+      props: true
+    },
+    {
+      path: '/food',
+      name: 'FoodListView',
+      component: FoodListView,
+      props: true
+    },
+
+    {
+      path: '/food/:id',
+      name: 'FoodView',
+      component: FoodView,
+      props: true
+    }
   ],
   linkActiveClass: "active"
 })
