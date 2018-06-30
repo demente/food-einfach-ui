f5bf0c736297b7b9c0d176181f9d33bf46ab7668
demente <olga.agen@gmail.com>
2018-06-30 21:54:57 +0200

    Add food view

---
+++ src/views/FoodView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        {{food.name}}
+        <NutritionalInfo :nutrients=food.NutritionalInfo />
+        </div>
+</template>
+
+<script>
+import axios from 'axios';
+import NutritionalInfo from '../components/NutritionalInfo'
+
+export default {
+  name: "FoodView",
+  props: ["id"],
+  components: {NutritionalInfo},
+  data() {
+    return {
+      food: {}
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8080/food/"+this.id)
+      .then(response => this.updateFood(response));
+  },
+  methods: {
+    updateFood(response) {
+      this.food = response.data;
+    }
+  }
+};
+</script>
