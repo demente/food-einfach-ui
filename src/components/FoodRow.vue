6c057bf72abbee3f0a54485a672d80472ffcf032
demente <olga.agen@gmail.com>
2018-06-30 21:54:22 +0200

    Add link to food view

--- src/components/FoodRow.vue
+++ src/components/FoodRow.vue
@@ -1,13 +1,15 @@
 <template>
 <div class="row">
-<div class="col">{{food.name}}</div>
+<div class="col">
+	<router-link :to="{name:'FoodView', params: {id: food.id}}">{{food.name}}</router-link>
+	</div>
 <div class="col">{{food.type}}</div>
 </div>
 </template>
 
 <script>
-export default{
-	name: 'FoodRow',
-	props: ['food']
-}
+export default {
+  name: "FoodRow",
+  props: ["food"]
+};
 </script>
