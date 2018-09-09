53545dd89e2780f7e0e5ee7891f2b2760ac0cbc7
demente <olga.agen@gmail.com>
2018-09-09 12:23:59 +0200

    Add nutritional information service to calculate totals and averages of nutritional info

--- src/utils/NutritionalInformationService.js
+++ src/utils/NutritionalInformationService.js
@@ -1,89 +1,168 @@
-export default class NutritionalInformation {
-    
-    average(recipeList) {
-        var nutritionsList = [];
-        for (var i in recipeList) {
-          var recipe = recipeList[i].recipe;
-          console.log("The outcome to be divided by number of servings: " + recipe.servings)
-          
-          }
-        
-        return [ ]
+export default class NutritionalInformationService {
+  /**
+   * @param {*} recipeList - list of recipes
+   * @returns nutritionalInformation
+   */
+  average (recipeList) {
+    var nutritionalInfoPerRecipe = []
+    for (var i in recipeList) {
+      var recipe = recipeList[i]
+      nutritionalInfoPerRecipe.push(this.perRecipeServing(recipe))
+    }
+    var totals = this.total(nutritionalInfoPerRecipe)
+    return this.dividedBy(totals, recipeList.length)
+  }
+  /**
+   * @param {*} recipe
+   * @returns nutritional information in one serving
+   */
+  perRecipeServing (recipe) {
+    var perIngredient = []
+
+    for (var j in recipe.ingredients) {
+      var ingredient = recipe.ingredients[j]
+
+      var timesHundredGrams = ingredient.amount.weight / 100000000
+
+      var info = {}
+      info.calories = ingredient.food.nutritionalInformation.calories * timesHundredGrams
+      info.carbohydrates = { weight: this.toMicrogram(ingredient.food.nutritionalInformation.carbohydrates) * timesHundredGrams, unit: 'MICROGRAM' }
+      info.fat = { weight: this.toMicrogram(ingredient.food.nutritionalInformation.fat) * timesHundredGrams, unit: 'MICROGRAM' }
+      info.fibre = { weight: this.toMicrogram(ingredient.food.nutritionalInformation.fibre) * timesHundredGrams, unit: 'MICROGRAM' }
+      info.protein = { weight: this.toMicrogram(ingredient.food.nutritionalInformation.protein) * timesHundredGrams, unit: 'MICROGRAM' }
+      info.sugar = { weight: this.toMicrogram(ingredient.food.nutritionalInformation.sugar) * timesHundredGrams, unit: 'MICROGRAM' }
+      info.vitamins = this.elementsMultipliedBy(ingredient.food.nutritionalInformation.vitamins, timesHundredGrams)
+      info.minerals = this.elementsMultipliedBy(ingredient.food.nutritionalInformation.minerals, timesHundredGrams)
+
+      perIngredient.push(info)
+    }
+
+    var total = this.total(perIngredient)
+    return this.dividedBy(total, recipe.servings)
+  }
+  /**
+   * @param {*} nutritionalInformationList list of nutritionalInfo
+   * @returns nutritionalInfo
+   */
+  total (nutritionalInformationList) {
+    var total = {
+      calories: 0,
+      protein: { 'weight': 0, unit: 'MICROGRAM' },
+      fat: { 'weight': 0, unit: 'MICROGRAM' },
+      carbohydrates: { 'weight': 0, unit: 'MICROGRAM' },
+      sugar: { 'weight': 0, unit: 'MICROGRAM' },
+      fibre: { 'weight': 0, unit: 'MICROGRAM' },
+      minerals: [],
+      vitamins: []
+    }
+    for (var i in nutritionalInformationList) {
+      var info = nutritionalInformationList[i]
+      total.calories += info.calories
+      total.protein.weight += this.toMicrogram(info.protein)
+      total.fat.weight += this.toMicrogram(info.fat)
+      total.carbohydrates.weight += this.toMicrogram(info.carbohydrates)
+      total.sugar.weight += this.toMicrogram(info.sugar)
+      total.fibre.weight += this.toMicrogram(info.fibre)
+
+      total.minerals = this.addElement(total.minerals, info.minerals)
+      total.vitamins = this.addElement(total.vitamins, info.vitamins)
+    }
+    return total
+  }
+
+  /**
+   *
+   * @param {*} totals list of element with total weight
+   * @param {*} elements list of elements to be added to totals list
+   * @returns totals
+   */
+  addElement (totals, elements) {
+    for (var i in elements) {
+      var element = elements[i]
+      var elementTotal = this.elementWithName(totals, element.name)
+      if (elementTotal) {
+        elementTotal.amount.weight += this.toMicrogram(element.amount)
+      } else {
+        element.amount.weight = this.toMicrogram(element.amount)
+        element.amount.unit = 'MICROGRAM'
+        totals.push(element)
       }
-
-      perRecipeServing(recipe){
-        
-        var perIngredient = [];
-
-        for (var j in recipe.ingredients) {
-            var ingredient = recipe.ingredients[j];
-        
-            var timesHundredGrams = ingredient.amount.weight / 100000000
-
-            var info = {}
-            info.calories = ingredient.food.nutritionalInformation.calories * timesHundredGrams 
-            info.carbohydrates = ingredient.food.nutritionalInformation.carbohydrates.weight * timesHundredGrams 
-            info.fat = ingredient.food.nutritionalInformation.fat.weight * timesHundredGrams 
-            info.fibre = ingredient.food.nutritionalInformation.fibre.weight * timesHundredGrams 
-            info.protein =ingredient.food.nutritionalInformation.protein.weight * timesHundredGrams 
-            info.sugar = ingredient.food.nutritionalInformation.sugar.weight * timesHundredGrams 
-
-            perIngredient.push(info)
-        }
-
-        var total = this.calculateTotal(perIngredient)
-        return this.divideAllBy(total, recipe.servings)
-
-    } 
-    
-    total(nutritionalInformationList){
-        var total = {
-            calories: 0,
-            carbohydrates: 0,
-            fat: 0,
-            fibre: 0,
-            protein: 0,
-            sugar: 0
-        }
-        for(var i in nutritionalInformationList){
-            var info = nutritionalInformationList[i]
-            total.calories += info.calories
-            total.carbohydrates +=info.carbohydrates
-            total.fat += info.fat
-            total.fibre += info.fibre
-            total.protein += info.protein
-            total.sugar += info.sugar
-        }
-        return total;
     }
-
-    dividedBy(nutritionalInformation, divider){
-        var divided = {};
-        divided.calories = nutritionalInformation.calories / divider
-        divided.protein = {weight: nutritionalInformation.protein.weight / divider, unit: nutritionalInformation.protein.unit}
-        divided.fat = {weight: nutritionalInformation.fat.weight / divider, unit: nutritionalInformation.fat.unit}
-        divided.carbohydrates = { weight: nutritionalInformation.carbohydrates.weight / divider, unit: nutritionalInformation.carbohydrates.unit}
-        divided.sugar =  {weight:nutritionalInformation.sugar.weight / divider, unit: nutritionalInformation.sugar.unit}
-        divided.fibre =  {weight: nutritionalInformation.fibre.weight / divider, unit: nutritionalInformation.fibre.unit}
-        divided.vitamins = this.elementsDividedBy(nutritionalInformation.vitamins, divider)
-        divided.minerals = this.elementsDividedBy(nutritionalInformation.minerals, divider)
-        return divided;
+    return totals
+  }
+  /**
+   * @param {*} elements list of elements
+   * @param {*} name name of the element to return
+   */
+  elementWithName (elements, name) {
+    for (var i in elements) {
+      var element = elements[i]
+      if (element.name === name) {
+        return element
+      }
     }
+  }
 
-    elementsDividedBy(elements, divider){
-        var divided = []
-        for(var i in elements){
-            var vitamin = elements[i]
-            var dividedVitamin = { name: vitamin.name,
-            amount: {
-              weight: vitamin.amount.weight / divider,
-              unit: vitamin.amount.unit
-            },
-            dailyNorm: vitamin.dailyNorm
-            }
-            divided.push(dividedVitamin)
-        }
-        return divided
+  /**
+   *
+   * @param {*} nutritionalInformation
+   * @param {*} divider
+   * @returns nutritional info
+   */
+  dividedBy (nutritionalInformation, divider) {
+    var divided = {}
+    divided.calories = nutritionalInformation.calories / divider
+    divided.protein = { weight: nutritionalInformation.protein.weight / divider, unit: nutritionalInformation.protein.unit }
+    divided.fat = { weight: nutritionalInformation.fat.weight / divider, unit: nutritionalInformation.fat.unit }
+    divided.carbohydrates = { weight: nutritionalInformation.carbohydrates.weight / divider, unit: nutritionalInformation.carbohydrates.unit }
+    divided.sugar = { weight: nutritionalInformation.sugar.weight / divider, unit: nutritionalInformation.sugar.unit }
+    divided.fibre = { weight: nutritionalInformation.fibre.weight / divider, unit: nutritionalInformation.fibre.unit }
+    divided.vitamins = this.elementsDividedBy(nutritionalInformation.vitamins, divider)
+    divided.minerals = this.elementsDividedBy(nutritionalInformation.minerals, divider)
+    return divided
+  }
+
+  /**
+   *
+   * @param {*} elements  list of elements
+   * @param {*} divider number to divide weight by
+   * @returns list of elements
+   */
+  elementsDividedBy (elements, divider) {
+    var divided = []
+    for (var i in elements) {
+      var element = elements[i]
+      var dividedElement = {
+        name: element.name,
+        amount: {
+          weight: element.amount.weight / divider,
+          unit: element.amount.unit
+        },
+        dailyNorm: element.dailyNorm
+      }
+      divided.push(dividedElement)
     }
+    return divided
+  }
 
+  elementsMultipliedBy (elements, multiplier) {
+    return this.elementsDividedBy(elements, 1 / multiplier)
+  }
+
+  /**
+   *
+   * @param {*} elemen
+   * @returns weight of element in micrograms
+   */
+  toMicrogram (element) {
+    if (element.unit === 'MICROGRAM') {
+      return element.weight
+    }
+    if (element.unit === 'GRAM') {
+      return element.weight * 1000000
+    }
+    if (element.unit === 'MILLIGRAM') {
+      return element.weight * 1000
+    }
+  }
 }
--- test/unit/specs/NutritionalInformationService.spec.js
+++ test/unit/specs/NutritionalInformationService.spec.js
@@ -1,117 +1,348 @@
-
 import NutritionalInformationService from '@/utils/NutritionalInformationService.js'
 
 describe('NutritionalInformationService.js', () => {
-    const service = new NutritionalInformationService();
-    
-    const expected = {
-        "calories": 52,
-        "protein": {
-          "weight": 100,
-          "unit": "MICROGRAM"
-        },
-        "fat": {
-          "weight": 100,
-          "unit": "MICROGRAM"
-        },
-        "carbohydrates": {
-          "weight": 100,
-          "unit": "MICROGRAM"
-        },
-        "sugar": {
-          "weight": 100,
-          "unit": "MICROGRAM"
-        },
-        "fibre": {
-          "weight": 100,
-          "unit": "MICROGRAM"
-        },
-        "vitamins": [
-          {
-            "name": "A",
-            "amount": {
-              "weight": 100,
-              "unit": "MICROGRAM"
-            },
-            "dailyNorm": {
-              "weight": 100,
-              "unit": "MICROGRAM"
-            }
-          }
-        ],
-        "minerals": [
-          {
-            "name": "Ca",
-            "amount": {
-            "weight": 100,
-            "unit": "MICROGRAM"
-            },
-            "dailyNorm": {
-              "weight": 100,
-              "unit": "MICROGRAM"
-            }
-          }
-        ]
-      }
+  const service = new NutritionalInformationService()
+  const vitamin = {
+    'name': 'A',
+    'amount': {
+      'weight': 100,
+      'unit': 'MICROGRAM'
+    },
+    'dailyNorm': {
+      'weight': 500,
+      'unit': 'MICROGRAM'
+    }
+  }
+  const mineral = {
+    'name': 'Ca',
+    'amount': {
+      'weight': 100,
+      'unit': 'MICROGRAM'
+    },
+    'dailyNorm': {
+      'weight': 500,
+      'unit': 'MICROGRAM'
+    }
+  }
 
+  const nutritionalInfo = {
+    'calories': 60,
+    'protein': {
+      'weight': 100,
+      'unit': 'MICROGRAM'
+    },
+    'fat': {
+      'weight': 100,
+      'unit': 'MICROGRAM'
+    },
+    'carbohydrates': {
+      'weight': 100,
+      'unit': 'MICROGRAM'
+    },
+    'sugar': {
+      'weight': 100,
+      'unit': 'MICROGRAM'
+    },
+    'fibre': {
+      'weight': 100,
+      'unit': 'MICROGRAM'
+    },
+    'vitamins': [
+      vitamin
+    ],
+    'minerals': [
+      mineral
+    ]
+  }
 
-  it('returns calculated average', () => {
-    var actual = service.average([])
-    expect(actual).to.deep.equal([])
-  })
+  const ingredient = {
+    'id': 1,
+    'food': {
+      'id': 2,
+      'name': 'Apples',
+      'nutritionalInformation': nutritionalInfo
+    },
+    'amount': {
+      'weight': 400000000,
+      'unit': 'MICROGRAM'
+    }
+  }
+
+  const recipe = {
+    'id': 1,
+    'name': 'My recipe',
+    'ingredients': [
+      ingredient
+    ],
+    'servings': 4
+  }
 
   it('divides all nutrients by divider', () => {
     var original = {
-        "calories": 520,
-        "protein": {
-          "weight": 1000,
-          "unit": "MICROGRAM"
-        },
-        "fat": {
-          "weight": 1000,
-          "unit": "MICROGRAM"
-        },
-        "carbohydrates": {
-          "weight": 1000,
-          "unit": "MICROGRAM"
-        },
-        "sugar": {
-          "weight": 1000,
-          "unit": "MICROGRAM"
-        },
-        "fibre": {
-          "weight": 1000,
-          "unit": "MICROGRAM"
-        },
-        "vitamins": [
-          {
-            "name": "A",
-            "amount": {
-              "weight": 1000,
-              "unit": "MICROGRAM"
-            },
-            "dailyNorm": {
-              "weight": 100,
-              "unit": "MICROGRAM"
-            }
+      'calories': 600,
+      'protein': {
+        'weight': 1000,
+        'unit': 'MICROGRAM'
+      },
+      'fat': {
+        'weight': 1000,
+        'unit': 'MICROGRAM'
+      },
+      'carbohydrates': {
+        'weight': 1000,
+        'unit': 'MICROGRAM'
+      },
+      'sugar': {
+        'weight': 1000,
+        'unit': 'MICROGRAM'
+      },
+      'fibre': {
+        'weight': 1000,
+        'unit': 'MICROGRAM'
+      },
+      'vitamins': [
+        {
+          'name': 'A',
+          'amount': {
+            'weight': 1000,
+            'unit': 'MICROGRAM'
+          },
+          'dailyNorm': {
+            'weight': 500,
+            'unit': 'MICROGRAM'
           }
-        ],
-        "minerals": [
-          {
-            "name": "Ca",
-            "amount": {
-            "weight": 1000,
-            "unit": "MICROGRAM"
-            },
-            "dailyNorm": {
-              "weight": 100,
-              "unit": "MICROGRAM"
-            }
+        }
+      ],
+      'minerals': [
+        {
+          'name': 'Ca',
+          'amount': {
+            'weight': 1000,
+            'unit': 'MICROGRAM'
+          },
+          'dailyNorm': {
+            'weight': 500,
+            'unit': 'MICROGRAM'
           }
-        ]
+        }
+      ]
+    }
+
+    expect(service.dividedBy(original, 10)).to.deep.equal(nutritionalInfo)
+  })
+
+  it('calculates nutritional information per recipe serving', () => {
+    var apple = {
+      'id': 1,
+      'food': {
+        'id': 2,
+        'name': 'Apples',
+        'nutritionalInformation': nutritionalInfo
+      },
+      'amount': {
+        'weight': 100000000,
+        'unit': 'MICROGRAM'
       }
+    }
+    var actual = {
+      'id': 1,
+      'name': 'My recipe',
+      'ingredients': [
+        apple
+      ],
+      'servings': 2
+    }
 
-      expect(service.dividedBy(original,10)).to.deep.equal(expected)
+    const expected = {
+      'calories': 30,
+      'carbohydrates': {
+        'unit': 'MICROGRAM',
+        'weight': 50
+      },
+      'fat': {
+        'unit': 'MICROGRAM',
+        'weight': 50
+      },
+      'fibre': {
+        'unit': 'MICROGRAM',
+        'weight': 50
+      },
+      'minerals': [
+        {
+          'amount': {
+            'unit': 'MICROGRAM',
+            'weight': 50
+          },
+          'dailyNorm': {
+            'unit': 'MICROGRAM',
+            'weight': 500
+          },
+          'name': 'Ca'
+        }
+      ],
+      'protein': {
+        'unit': 'MICROGRAM',
+        'weight': 50
+      },
+      'sugar': {
+        'unit': 'MICROGRAM',
+        'weight': 50
+      },
+      'vitamins': [
+        {
+          'amount': {
+            'unit': 'MICROGRAM',
+            'weight': 50
+          },
+          'dailyNorm': {
+            'unit': 'MICROGRAM',
+            'weight': 500
+          },
+          'name': 'A'
+        }
+      ]
+    }
 
+    expect(service.perRecipeServing(actual)).to.deep.equal(expected)
+  })
+
+  it('calculate total nutritional information', () => {
+    const actual = {
+      'calories': 30,
+      'protein': {
+        'weight': 50,
+        'unit': 'MICROGRAM'
+      },
+      'fat': {
+        'weight': 50,
+        'unit': 'MICROGRAM'
+      },
+      'carbohydrates': {
+        'weight': 50,
+        'unit': 'MICROGRAM'
+      },
+      'sugar': {
+        'weight': 50,
+        'unit': 'MICROGRAM'
+      },
+      'fibre': {
+        'weight': 50,
+        'unit': 'MICROGRAM'
+      },
+      'vitamins': [
+        {
+          'name': 'A',
+          'amount': {
+            'weight': 50,
+            'unit': 'MICROGRAM'
+          },
+          'dailyNorm': {
+            'weight': 500,
+            'unit': 'MICROGRAM'
+          }
+        }
+      ],
+      'minerals': [
+        {
+          'name': 'Ca',
+          'amount': {
+            'weight': 50,
+            'unit': 'MICROGRAM'
+          },
+          'dailyNorm': {
+            'weight': 500,
+            'unit': 'MICROGRAM'
+          }
+        }
+      ]
+    }
+
+    expect(service.total([actual, actual])).to.deep.equal(nutritionalInfo)
+  })
+
+  it('divides elements by divider', () => {
+    const actual = [
+      {
+        'id': 1,
+        'name': 'Ca',
+        'amount': {
+          'weight': 6000,
+          'unit': 'MICROGRAM'
+        },
+        'dailyNorm': {
+          'weight': 1000000,
+          'unit': 'MICROGRAM'
+        }
+      }]
+    const expected = [
+      {
+        'name': 'Ca',
+        'amount': {
+          'weight': 600,
+          'unit': 'MICROGRAM'
+        },
+        'dailyNorm': {
+          'weight': 1000000,
+          'unit': 'MICROGRAM'
+        }
+      }
+    ]
+
+    expect(service.elementsDividedBy(actual, 10)).to.deep.equal(expected)
+  })
+
+  it('multiples elements by multiplier', () => {
+    const actual = [
+      {
+        'id': 1,
+        'name': 'Ca',
+        'amount': {
+          'weight': 60,
+          'unit': 'MICROGRAM'
+        },
+        'dailyNorm': {
+          'weight': 1000000,
+          'unit': 'MICROGRAM'
+        }
+      }]
+    const expected = [
+      {
+        'name': 'Ca',
+        'amount': {
+          'weight': 600,
+          'unit': 'MICROGRAM'
+        },
+        'dailyNorm': {
+          'weight': 1000000,
+          'unit': 'MICROGRAM'
+        }
+      }
+    ]
+
+    expect(service.elementsMultipliedBy(actual, 10)).to.deep.equal(expected)
+  })
+
+  it('returns weight converted to micrograms', () => {
+    const elementInMilligram = { weight: 20, unit: 'MILLIGRAM' }
+    const elementInGram = { weight: 20, unit: 'GRAM' }
+
+    expect(service.toMicrogram(elementInMilligram)).to.equal(20000)
+    expect(service.toMicrogram(elementInGram)).to.equal(20000000)
+  })
+
+  it('finds element with given name', () => {
+    var expected = { name: 'Ca' }
+    var elements = [{ name: 'Fe' }, { name: 'Cu' }, expected]
+    expect(service.elementWithName(elements, 'Ca')).to.equal(expected)
+  })
+
+  it('adds mineral to the list', () => {
+    var mineral = { name: 'Ca', amount: { weight: 10, unit: 'MICROGRAM' } }
+    expect(service.addElement([mineral], [mineral])).to.deep.equal([{ name: 'Ca', amount: { weight: 20, unit: 'MICROGRAM' } }])
+  })
+
+  it('converts element to microgram', () => {
+    var mineralInGrams = { name: 'Ca', amount: { weight: 0.00001, unit: 'GRAM' } }
+    expect(service.addElement([{ name: 'Ca', amount: { weight: 10, unit: 'MICROGRAM' } }], [mineralInGrams])).to.deep.equal([{ name: 'Ca', amount: { weight: 20, unit: 'MICROGRAM' } }])
   })
 })
