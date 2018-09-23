86099d9df4ab803087bbb625fe1098693cf3ee96
demente <olga.agen@gmail.com>
2018-09-23 12:13:34 +0200

    change design

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
 <div id="app">
 	<NavigationBar />
-	<div class="container">
+	<div class="container rounded  pt-2 pb-2" style="background-color:white;">
 		<router-view />
 	</div>
 </div>
 </template>
 
 <script>
-import 'bootstrap'
-import NavigationBar from './components/NavigationBar'
+import "bootstrap";
+import NavigationBar from "./components/NavigationBar";
 
 export default {
-  name: 'App',
-  components: {NavigationBar}
-}
+  name: "App",
+  components: { NavigationBar }
+};
 </script>
 
 
 <style lang="scss">
-$fa-font-path:"../node_modules/font-awesome/fonts";
-@import './assets/css/minty.css';
-@import '../node_modules/font-awesome/scss/font-awesome.scss';
+$fa-font-path: "../node_modules/font-awesome/fonts";
+@import "./assets/css/minty.css";
+@import "../node_modules/font-awesome/scss/font-awesome.scss";
 </style>
